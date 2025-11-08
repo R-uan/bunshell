15169779b762a36088c8f1c5208ecad65b50f6b0
Ruan <ruan.oliveiralopes@hotmail.com>
2025-11-08 14:53:45 -0300

    utils: del handler

--- src/utils.c
+++ src/utils.c
@@ -16,7 +16,7 @@ void insert_char(char *buffer, int *len, int *cursorPos, char c) {
   fflush(stdout);
 }
 
-void delete_char(char *buffer, int *index, int *cursor) {
+void backspace(char *buffer, int *index, int *cursor) {
   if (*cursor == 0 || *index == 0)
     return;
 
@@ -42,6 +42,22 @@ void delete_char(char *buffer, int *index, int *cursor) {
   }
 }
 
+void del(char *buffer, int *index, int *cursor) {
+  if (*cursor >= *index)
+    return;
+  memmove(&buffer[*cursor], &buffer[*cursor + 1], *index - *cursor);
+  (*index)--;
+
+  int charsAfter = *index - *cursor;
+  write(STDOUT_FILENO, &buffer[*cursor], *index - *cursor);
+  write(STDOUT_FILENO, " ", 1);
+  char move_seq[16];
+  snprintf(move_seq, sizeof(move_seq), "\033[%dD", charsAfter + 1);
+  write(STDOUT_FILENO, move_seq, strlen(move_seq));
+
+  fflush(stdout);
+}
+
 char *read_input_raw() {
   int index = 0;
   int bufferSize = 1024;
@@ -61,7 +77,7 @@ char *read_input_raw() {
     }
 
     if (c == 127 || c == 8) {
-      delete_char(buffer, &index, &cursorPosition);
+      backspace(buffer, &index, &cursorPosition);
       continue;
     }
 
@@ -70,7 +86,7 @@ char *read_input_raw() {
     }
 
     if (c == 27) {
-      char seq[2];
+      char seq[3];
       if (read(STDIN_FILENO, &seq[0], 1) != 1)
         continue;
       if (read(STDIN_FILENO, &seq[1], 1) != 1)
@@ -83,6 +99,10 @@ char *read_input_raw() {
         } else if (seq[1] == 'D' && cursorPosition > 0) {
           cursorPosition--;
           write(STDOUT_FILENO, "\033[D", 3);
+        } else if (seq[1] == '3') {
+          read(STDIN_FILENO, &seq[2], 1);
+          if (seq[2] == '~')
+            del(buffer, &index, &cursorPosition);
         }
       }
       continue;
