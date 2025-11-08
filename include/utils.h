0d0528f90f4a785cebac51a99811cf104a7229f4
Ruan <ruan.oliveiralopes@hotmail.com>
2025-11-07 23:58:51 -0300

    utils: delete char

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-char *read_input();
 char *read_input_raw();
 char **tokenize(char *input);
 
--- src/utils.c
+++ src/utils.c
@@ -5,33 +5,47 @@
 #include <string.h>
 #include <unistd.h>
 
-char *read_input() {
-  char c;
-  int index = 0;
-  int bufferSize = 1024;
-  char *buffer = malloc(bufferSize * sizeof(char));
+void insert_char(char *buffer, int *len, int *cursorPos, char c) {
+  memmove(&buffer[*cursorPos + 1], &buffer[*cursorPos], *len - *cursorPos);
+  buffer[*cursorPos] = c;
+  (*len)++;
 
-  while ((c = getchar()) != EOF && c != '\n') {
-    if (index >= bufferSize) {
-      bufferSize *= 2;
-      char *tempBuffer = realloc(buffer, bufferSize * sizeof(char));
-      if (tempBuffer == NULL) {
-        printf("bunshell: realloc failed\n");
-        free(buffer);
-        exit(1);
-      }
-      buffer = tempBuffer;
-    }
-    buffer[index++] = c;
+  write(STDOUT_FILENO, &buffer[*cursorPos], *len - *cursorPos);
+  (*cursorPos)++;
+  printf("\033[%dD", *len - *cursorPos); // Move left
+  fflush(stdout);
+}
+
+void delete_char(char *buffer, int *index, int *cursor) {
+  if (*cursor == 0 || *index == 0)
+    return;
+
+  if (*cursor != *index) {
+    memmove(&buffer[*cursor - 1], &buffer[*cursor], *index - *cursor);
+    (*cursor)--;
+    (*index)--;
+
+    int charsAfter = *index - *cursor;
+    write(STDOUT_FILENO, "\033[D", 3);
+    write(STDOUT_FILENO, &buffer[*cursor], charsAfter);
+    write(STDOUT_FILENO, " ", 1);
+
+    char move_seq[16];
+    snprintf(move_seq, sizeof(move_seq), "\033[%dD", charsAfter + 1);
+    write(STDOUT_FILENO, move_seq, strlen(move_seq));
+
+    fflush(stdout);
+  } else {
+    (*index)--;
+    (*cursor)--;
+    write(STDOUT_FILENO, "\b \b", 3);
   }
-
-  buffer[index] = '\0';
-  return buffer;
 }
 
 char *read_input_raw() {
   int index = 0;
   int bufferSize = 1024;
+  int cursorPosition = 0;
   char *buffer = malloc(bufferSize * sizeof(char));
 
   if (buffer == NULL) {
@@ -47,15 +61,31 @@ char *read_input_raw() {
     }
 
     if (c == 127 || c == 8) {
-      if (index > 0) {
-        index--;
-        write(STDOUT_FILENO, "\b \b", 3);
-      }
+      delete_char(buffer, &index, &cursorPosition);
       continue;
     }
 
     if (c == 3) {
       exit(1);
+    }
+
+    if (c == 27) {
+      char seq[2];
+      if (read(STDIN_FILENO, &seq[0], 1) != 1)
+        continue;
+      if (read(STDIN_FILENO, &seq[1], 1) != 1)
+        continue;
+
+      if (seq[0] == '[') {
+        if (seq[1] == 'C' && cursorPosition < index) {
+          cursorPosition++;
+          write(STDOUT_FILENO, "\033[C", 3);
+        } else if (seq[1] == 'D' && cursorPosition > 0) {
+          cursorPosition--;
+          write(STDOUT_FILENO, "\033[D", 3);
+        }
+      }
+      continue;
     }
 
     if (iscntrl(c) && c != '\t') {
@@ -73,8 +103,13 @@ char *read_input_raw() {
       buffer = tempBuffer;
     }
 
-    write(STDOUT_FILENO, &c, 1);
-    buffer[index++] = c;
+    if (cursorPosition != index) {
+      insert_char(buffer, &index, &cursorPosition, c);
+    } else {
+      write(STDOUT_FILENO, &c, 1);
+      buffer[index++] = c;
+      cursorPosition++;
+    }
   }
 
   buffer[index] = '\0';
